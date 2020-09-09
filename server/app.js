3fcc362cf953ce17340c7231ab29c42bfb1519d2
zouxuchao <1150171730@qq.com>
2020-09-09 13:15:48 +0800

    add server

---
+++ server/app.js
@@ -0,0 +1,33 @@
+// var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+let expressArtTemplate = require('express-art-template')
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));  //set views path
+app.engine('.html', expressArtTemplate) 
+app.set('view engine', '.html');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+//error
+app.use(function(req,res){
+  res.status(500).render('error.html')
+})
+
+module.exports = app;
